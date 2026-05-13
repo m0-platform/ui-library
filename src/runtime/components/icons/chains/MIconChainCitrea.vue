619f325beba49a470224f584ceb847c11a0de3cb
Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>
2026-05-13 11:25:03 -0500

    feat(chains): support citrea, moca and fluent

---
+++ src/runtime/components/icons/chains/MIconChainCitrea.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  size?: string | number;
+  alt?: string;
+}>();
+</script>
+
+<template>
+  <img
+    src="https://green-obedient-albatross-155.mypinata.cloud/ipfs/bafkreidw7akutkkrhmywnynoi736mirnbi34uhq577r5hsumz2psfqyfye"
+    :width="size ?? 24"
+    :height="size ?? 24"
+    :alt="alt || 'Soneium'"
+    class="rounded-full"
+  />
+</template>
